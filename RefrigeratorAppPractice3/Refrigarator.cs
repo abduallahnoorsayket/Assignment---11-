5e7b6c975046ba5a40d39d61629d6c6f3adf8e3a
Abdullah noor sayket <abdullahnoorsayket@gmail.com>
2019-10-14 23:33:46 +0600

    Second Commit

---
+++ RefrigeratorAppPractice3/Refrigarator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefrigeratorAppPractice3
+{
+  public  class Refrigarator
+    {
+       public double MaxWeight { set; get; }
+        public int NoItem { set; get; }
+        public double UnitWeight { set; get; }
+    }
+}
---
+++ RefrigeratorAppPractice3/RefrigeratorUi.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RefrigeratorAppPractice3
+{
+    public partial class RefrigeratorUi : Form
+    {
+
+        Refrigarator refrigarator = new Refrigarator();
+        public RefrigeratorUi()
+        {
+            InitializeComponent();
+        }
+
+        private void SaveButton_Click(object sender, EventArgs e)
+        {
+            refrigarator.MaxWeight = Convert.ToDouble(maxWeightTakeTextBox.Text);
+        }
+
+
+
+
+
+    }
+}
